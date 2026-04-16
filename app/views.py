19f86cd91d23fff67e8ec46380138d2967893966
Jeim <159331654+Jeimtax@users.noreply.github.com>
2026-04-16 13:14:24 -0500

    Finished exercise 3

--- app/views.py
+++ app/views.py
@@ -5,9 +5,13 @@ Werkzeug Documentation:  https://werkzeug.palletsprojects.com/
 This file creates your application.
 """
 
-from app import app
+from app import app, db
 from flask import render_template, request, jsonify, send_file
 import os
+from app.models import Movies
+from app.forms import MovieForm
+from werkzeug.utils import secure_filename
+from datetime import datetime
 
 
 ###
@@ -18,6 +22,30 @@ import os
 def index():
     return jsonify(message="This is the beginning of our API")
 
+@app.route('/api/v1/movies', methods=['POST'])
+def movies():
+    form = MovieForm
+    if form.validate_on_submit():
+        title = form.title.data
+        description = form.description.data
+        poster = form.poster.data
+
+        filename = secure_filename(poster.filename)
+        poster.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        movie = Movies(title=title, description=description, poster=filename)
+        movie.created_at = datetime.now()
+        db.session.add(movie)
+        db.session.commit()
+        
+        return jsonify({
+            'message': 'Movie Successfully added',
+            'title': title,
+            'poster': filename,
+            'description': description
+        })
+    
+    return jsonify({'errors': form_errors(form)}), 400
 
 ###
 # The functions below should be applicable to all Flask apps.

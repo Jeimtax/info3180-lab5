e781c09f4a4bc85a3e03dccd92e460d7fd8d4bc3
Jeim <159331654+Jeimtax@users.noreply.github.com>
2026-04-16 12:15:19 -0500

    Added movie model and setup migrations

--- app/models.py
+++ app/models.py
@@ -1,1 +1,16 @@
-# Add any model classes for Flask-SQLAlchemy here
+from app import db
+from datetime import datetime
+
+class Movies(db.Model):
+    __tablename__ = 'movies'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    description = db.Column(db.Text)
+    poster = db.Column(db.String(80))
+    created_at = db.Column(db.DateTime, default=datetime.now)
+
+    def __init__(self, title, description, poster):
+        self.title = title
+        self.description = description
+        self.poster = poster
+
